15d371a31bc9964047b8fa557109362643e4c282
Isha Sharma <86165115+ishyyyy@users.noreply.github.com>
2022-08-08 22:16:24 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 22:30:22 2021
+
+@author: ishur
+"""
+
+n=int(input("enter the number"))
+value=0
+for sum in range(2,n+1):
+    sum=(sum-1)+(sum-2)
+    print(sum)
+  
